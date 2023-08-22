bb15454800d5eeebbac879ed72d5b81396251a72
Israel-Ferreira <israelfsouza@hotmail.com>
2023-08-22 17:13:04 -0300

    feature: Leitura de Arquivos em C#

---
+++ ByteBankIO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using ByteBankIO;
+
+namespace ByteBankIO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        var numeroDeBytesLidos = -1;
+        
+        string enderecoArquivo = "contas.txt";
+        
+        var streamArquivo =  new FileStream(enderecoArquivo, FileMode.Open,FileAccess.Read);
+        
+        byte[] buffer = new byte[1024]; // Buffer de 1KB
+        
+        while(numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = streamArquivo.Read(buffer, 0, 1024);
+            PrintBuffer(buffer);
+        }
+        
+        Console.ReadLine();
+    }
+    
+    
+    static void PrintBuffer(byte[] buffer)
+    {
+        UTF8Encoding utf8 =  new UTF8Encoding();
+        string text  =  utf8.GetString(buffer);
+        
+        Console.Write(text);
+    }
+}
---
+++ ExercLeituraArquivo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace ExercLeituraArquivo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string pathFile =  "arquivo.txt";
+        
+        using (var fs =  new FileStream(pathFile, FileMode.Open, FileAccess.Read))
+        {
+            byte[] buffer =  new byte[1024];
+            var encoding =  Encoding.ASCII;
+            
+            var bytesLidos = fs.Read(buffer, 0, 1024);
+            var fileContent = encoding.GetString(buffer, 0, buffer.Length);
+            
+            Console.WriteLine(fileContent);
+        }
+    }
+}
